7f087b46a974e6f30eb61b7f408b18d2b0757890
Skar13F <of5162@gmail.com>
2023-10-20 18:10:32 -0600

    agregado de la clase conexion a la base de datos y la clase administradores model para gestionar la insercion de los mismos

---
+++ src/main/java/conection/Conexion.java
@@ -0,0 +1,50 @@
+
+package conection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    // Configura la información de la base de datos
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USUARIO = "postgres";
+    private static final String CONTRASENA = "";
+
+    public static Connection obtenerConexion() {
+        Connection conexion = null;
+
+        try {
+            // Carga el controlador JDBC de PostgreSQL
+            Class.forName("org.postgresql.Driver");
+            
+            // Establece la conexión a la base de datos
+            conexion = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+
+            if (conexion != null) {
+                System.out.println("Conexión exitosa a la base de datos PostgreSQL.");
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: No se pudo cargar el controlador JDBC.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Error: No se pudo establecer la conexión a la base de datos.");
+            e.printStackTrace();
+        }
+
+        return conexion;
+    }
+
+    public static void cerrarConexion(Connection conexion) {
+        try {
+            if (conexion != null) {
+                conexion.close();
+                System.out.println("Conexión cerrada.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al cerrar la conexión.");
+            e.printStackTrace();
+        }
+    }
+}
+
