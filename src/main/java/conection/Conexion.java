18fd162ca1bdef3da7049c1196ba95b553a9333c
labinfo06 <labinfo06@labinfo06>
2023-10-23 11:15:14 -0600

    UuU ToT

--- src/main/java/conection/Conexion.java
+++ src/main/java/conection/Conexion.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class Conexion {
     // Configura la información de la base de datos
-    private static final String URL = "jdbc:postgresql://localhost:5432/prueba1";
+    private static final String URL = "jdbc:postgresql://localhost:5432/inventario";
     private static final String USUARIO = "postgres";
     private static final String CONTRASENA = "root";
 
